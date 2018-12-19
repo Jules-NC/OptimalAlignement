2baa4fbcc5d0248303650d4f582865016b85acf3
Jules-NC <jules.nc@protonmail.com>
2018-12-19 02:48:24 +0100

    Fin Q3

--- TD2.c
+++ TD2.c
@@ -11,15 +11,14 @@ struct alignement
 
 
 int ** create_table(int X, int Y);
-int ** levenstein(char * x, char * y, int m, int n);
-void affiche_Table(int ** T, int x, int y);
+int ** levenstein(char *x, char * y, int m, int n);
+void affiche_Table(int **T, int x, int y);
 void affiche_backtrack(int **T, int x, int y);
-void backtrack(int **T, char* X, char* Y);
+struct alignement backtrack(int **T, char* X, char* Y);
 
 
-char * readtextfile(char * filename)
+char * readtextfile(char * filename){
   /* Retourne le contenu du fichier texte filename */
-{
   struct stat monstat;
   int N;
   char * text = NULL;
@@ -173,32 +172,50 @@ void affiche2(char* texte1, char* texte2, int nbcar){
 
 int main(int argc, char **argv){
   char *X, *Y; 
-  int x, y;
+  int x, y, lev_dist;
+  int ** T;
+  struct alignement Prime;
+
   
   if(argc != 3){
     printf("usage: %s text1 text2\n", argv[0]);
   }
   
-  X = "CHROAAAAAAAAAAAAAAAFFQSZAIEYGZUOPYUMOSOME";
-  Y = "SHTROUMPH";
+  X = readtextfile(argv[1]);
+  Y = readtextfile(argv[2]);
 
   x = strlen(X);
   y = strlen(Y);
-
-  int ** T;
-  T = levenstein(X, Y, x, y);
   
-  printf("Calcul de l'alignement optimal en O(x+y+1)\n");
-  affiche2(X, Y, Imax(x, y));
-  printf("\nAPRES ALIGNEMENT:\n\n");
-  backtrack(T, X, Y);
+  // 	Q1 : LEVENSTEIN DISTANCE (ya pas les options pour les coûts car inutile ici, je le met pas, tout est à 1)
+  T = levenstein(X, Y, x, y);
+  lev_dist = T[x][y];
+  
+  //	Q2-3 : ALIGNEMENT OPTIMAL X' et Y', commenté ds la fct°.
+  Prime = backtrack(T, X, Y);
+  
+  // Affichage
+  printf("Chaines de bases (X et Y):\n");
+  affiche(X, Y, 100);
+  printf("Distance d'édition: %d\n", lev_dist);
+  printf("\nAlignement optimal (X' et Y'):\n");
+  affiche(Prime.x, Prime.y, 100);//1 + Imax(strlen(Prime.x), strlen(Prime.y)));
 
+  free(X);
+  free(Y);
+  //	Celui-la je m'en souvenais pas tiens !
+  for(int i=0; i<=x; i++){
+	free(T[i]);
+  }
   free(T);
+  //free(Prime.x);
+  //free(Prime.y);
   return 0;
 }
 
 
 int ** create_table(int m, int n){
+	/*	Créé un tableau d'int de taille m*n	*/
   int ** T;
   T=malloc((m+1)*sizeof(int*));
   for(int i=0; i<m+1; i++)
@@ -208,6 +225,7 @@ int ** create_table(int m, int n){
 
 
 int ** levenstein(char * x, char * y, int m, int n){
+	/*	Oui, toutes les opérations sont à 1, parsque ca marche très bien comme ca, on travaille sur des textes, sacrebleu !!!!	*/
   int ** T = create_table(m,n);
   T[0][0] = 0;
   for(int j=1; j<=n; j++)
@@ -224,6 +242,7 @@ int ** levenstein(char * x, char * y, int m, int n){
       int sC = T[i][j-1] + 1;
       int tC = T[i-1][j] + 1;
       T[i][j] = Imin3(fC, sC, tC);
+      // Bien recopié !
     }
   }
   return T;
@@ -231,6 +250,7 @@ int ** levenstein(char * x, char * y, int m, int n){
 
 
 void affiche_Table(int ** T, int x, int y){
+	/*	Affiche... le tableau int** ...   ..     .*/
   printf("Lev:\n");  
   for(int i=0; i<=x;++i){
     for(int j=0;j<=y;++j)
@@ -240,21 +260,27 @@ void affiche_Table(int ** T, int x, int y){
 }
 
 
-void backtrack(int **T, char* X, char* Y){
+struct alignement backtrack(int **T, char* X, char* Y){
 	//	T : la matrice d'édition, x : taille x de T, y : taille y T
 	int x, y, xi, yi, haut, gauche, diag, min, max, i;
 	char *Xp, *Yp;
 	
-	// Initialisation des paramètres
-	x = strlen(X);
-	y = strlen(Y);
-	Xp = malloc((x+y+1)*sizeof(char));
-	Yp = malloc((x+y+1)*sizeof(char));
+	//	Initialisation des paramètres
+	x = strlen(X);							// 	|X|
+	y = strlen(Y);		
+						//	|Y|
+	//	On connait déjà la longueur max de X' et de Y' (alignement => |X'| = |Y'|), n délétions + n additions, mais bon en vrai je penche sur du Max(|X|, |Y|) + 1 pasque ca c'est des substitutions du cp
+	Xp = malloc((x+y+1)*sizeof(char));		// 	X'
+	Yp = malloc((x+y+1)*sizeof(char));		//	Y'
 	strcpy(Xp, "");
 	strcpy(Yp, "");
+	
+	//	 Compteurs pour trouver le bon char dans X et Y pour X' et Y'
 	xi = x-1;
 	yi = y-1;
-	max = T[x][y]+1;	// La distance d'édition va toujours être décroissante, donc on sait que T[x][y]+1 ne sera jamais dépassé (valeur initiale du backtrack => valeur maximale non stricte du chemin suivi par celui-ci)
+	
+	// 	La distance d'édition va toujours être décroissante, donc on sait que T[x][y]+1 ne sera jamais dépassé (valeur initiale du backtrack => valeur maximale non stricte du chemin suivi par celui-ci)
+	max = T[x][y]+1;	
 	i = 0;
 	
 	//	BACKTRACKING !!!
@@ -265,13 +291,16 @@ void backtrack(int **T, char* X, char* Y){
 		if(x!=0 && y!=0){diag = T[x-1][y-1];} else {diag = max;}
 		min = Imin3(gauche, haut, diag);	//	Valeur séléctionnée
 		
-		// Priorisation de la diagonale dans le backtracking => minimisation du nomrbe d'éditions (une subst = n*add + n*del du coup on choisit le chemin le + court)
-		if(min == diag){	
+		//	 Priorisation de la diagonale dans le backtracking => minimisation du nomrbe d'éditions (une subst = n*add + n*del du coup on choisit le chemin le + court)
+		if(min == diag){
 			Xp[i] = X[xi];
 			Yp[i] = Y[yi];
-			xi--; yi--;
-			x--; y--;
+			xi--;
+			yi--;
+			x--;
+			y--;
 		}
+		// 	La les 2 peuvent êtres inversés, l'idée c'est de ne pas se tromper sur ou mettre le caractere *
 		else if(min == haut){
 			Xp[i] = '*';
 			Yp[i] = Y[yi];
@@ -284,11 +313,17 @@ void backtrack(int **T, char* X, char* Y){
 			xi--;
 			x--;
 		}
+	// 	Et oui lui faut pas l'oublier, sert à itérer sur les chars de X' et Y' (à la fin, l'espace supplémentaire est utilisé)
 	i++;
 	}
+	//	Utilisé ici ! On met les \0 sinon C pas content
 	Xp[i] = '\0';
 	Yp[i] = '\0';
 	retourne(Xp);
 	retourne(Yp);
-	affiche2(Xp, Yp, 1+Imax(strlen(X), strlen(Y)));
+	
+	struct alignement res;
+	res.x = Xp;
+	res.y = Yp;
+	return res;
 }
